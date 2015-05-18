8b76c9c0d33ad7417e75e6e9cbd5ea62e2c23cb6
Matt Powers <mpowers@applicoinc.com>
2015-05-18 16:55:58 -0400

    finished up the templates and chapter 5

--- tests/index.js
+++ tests/index.js
@@ -3,6 +3,9 @@ var boot = require('../app').boot,
 	port = require('../app').port,
 	superagent = require('superagent'),
 	expect = require('expect.js');
+	
+var seedArticles = require('../db/articles.json');
+
 describe('server', function()
 {
 	
@@ -17,7 +20,48 @@ describe('server', function()
 				done()
 			})
 		})
+		
+		if('should contain posts', function(done)
+		{
+			superagent.get('http://localhost' + port).end(function(res)
+			{
+				seedArticles.forEach(function(item, index, list)
+				{
+					if(item.published)
+					{
+						expect(res.text).to.contain('<h2><a href="/articles/' + item.slug + '">' + item.title);
+					}
+					else 
+					{
+						expect(res.text).not.to.contain('<h2><a href="/articles/' + item.slug + '">' + item.title);	
+					}	
+				})
+				done()
+			})
+		});
 	});
+
+	describe('article page', function()
+	{
+		it('should display text', function(done)
+		{
+			var n = seedArticles.length;
+			seedArticles.forEach(function(item, index, list){
+				superagent.get('http://localhost:' + port + '/articles/' + seedArticles[index].slug).end(function(res)
+				{
+					if(item.published){
+						expect(res.text).to.contain(seedArticles[index].text)
+					}
+					else{
+						expect(res.status).to.be(401);
+					}
+					if(index + 1 === n){
+						done();
+					}
+				})
+			})
+		})
+	})
 
 	after(function(){
 		shutdown();
